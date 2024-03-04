1e2604ce644c4774e3f209ab2b3419aebbe893bd
Louis Pallett <louis.pallett@outlook.com>
2024-03-04 15:20:15 +0000

    Modify css to scss and set up file structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import Router from './Components/Router'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/index.scss';
+import Router from './Components/Router';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
