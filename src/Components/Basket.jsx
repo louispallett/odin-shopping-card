2028ab9025026bdb9ea8ef263ceb7bd8bccff750
Louis Pallett <louis.pallett@outlook.com>
2024-03-10 15:55:25 +0000

    Update positioning and wrap for Basket Component

--- src/Components/Basket.jsx
+++ src/Components/Basket.jsx
@@ -1,40 +1,62 @@
 import { useEffect, useState } from "react";
 
+import pattern from "../assets/pattern.jpg"
+
 export default function Basket({ itemsInBasket }) {
     return (
         <div className="basket-wrapper">
             <div className="basket-left">
-                {itemsInBasket.length !== 0 ? (
-                    itemsInBasket.map(item => (
-                        <BasketItem key={item.id} item={item} />
-                    ))
-                ) : (
-                    <div className="basket-item-wrapper">
-                        <p>Nothing in basket</p>
+                <h3>Items in Basket</h3>
+                <div>
+                    <div>
+                        {itemsInBasket.length !== 0 ? (
+                            itemsInBasket.map(item => (
+                                <BasketItem key={item.id} item={item} />
+                            ))
+                            ) : (
+                                <div className="basket-item-wrapper">
+                                    <p>Nothing in basket</p>
+                                </div>
+                            )
+                        }
                     </div>
-                )}
+                    <div className="pattern"></div>
+                </div>
             </div>
             <div className="basket-right">
-                <div className="basket-item-wrapper">
-                    <div className="basket-title">
-                        <h3>Checkout</h3>
+                <h3>Checkout</h3>
+                <form action="#">
+                    <div>
+                        <label htmlFor="firstName">First Name</label>
+                        <input type="text" id="firstName" />
+                        <label htmlFor="lastName">Last Name</label>
+                        <input type="text" id="lastName"/>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     )
 }
 
 function BasketItem({ item }) {
-
     return (
         <div className="basket-item-wrapper">
-            <div className="basket-title">
-                <h3>{item.title}</h3>
-            </div>
+            <img src={item.image} alt="" />
             <div className="basket-main">
-                <img src={item.image} alt="" />
-                <div>{item.description}</div>
+                <h3>{item.title}</h3>
+                <span>
+                    <h5>✓ In Stock</h5>
+                    <p className="price">£{item.price}</p>
+                </span>
+                <div className="price-add-wrapper">
+                    <div className="add-wrapper">
+                        <button id="minus-btn">-</button>
+                        <div type="text" className="quantity">5</div>
+                        <button id="plus-btn">+</button>
+                    </div>
+                    <button className="add">Update</button>
+                    <button className="remove">Remove</button>
+                </div>
             </div>
         </div>
     )
